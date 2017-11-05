ebefde9075066a40463818859b221c0f20508b05
Michael Ong <minghao.ong.2014@sis.smu.edu.sg>
2017-11-05 16:38:39 +0800

    final completed app, may add in more minor changes.

--- js/main.js
+++ js/main.js
@@ -1,22 +1,52 @@
 //**********************************
 //Misc Stuff
 //**********************************
-$(document).ready(function () {
+var nameList = [];
+var questionList = [];
 
+
+$(document).ready(function () {
+    retrieveData();
 })
 
 //This function is uses Google Sheets API to retrieve the information from the google sheets. 
 //It takes in timestamp, email, expertise and gender.
 function retrieveData() {
+    //recursive function to call retrieveData again.
+    setTimeout(retrieveData, 5000);
     $.ajax({
-        url: 'https://script.googleusercontent.com/macros/echo?user_content_key=x72DBi7PnP-krZmTlckfJ41U0-Va694QrJl83V61ltzpLJTjxFPiNnm-PbXP9E5jSFL1EiEG0ouTuGpHG1LdzFqsWYzNv3w8OJmA1Yb3SEsKFZqtv3DaNYcMrmhZHmUMWojr9NvTBuBLhyHCd5hHa1ZsYSbt7G4nMhEEDL32U4DxjO7V7yvmJPXJTBuCiTGh3rUPjpYM_V0PJJG7TIaKpzjnW7fDIdJvUc6S6oej-Uzzg-zYw7TrH--epZcnP5y2K8CuVI6Yu3Y5keLGnWsT4sgJ3ttE15malo-KUAPQVXM&lib=MbpKbbfePtAVndrs259dhPT7ROjQYJ8yx',
+        url: 'https://script.googleusercontent.com/macros/echo?user_content_key=z7Hxt6g9A7tSUb491Ba6hMBhmH0s2x65iaYDsMefVUywUQk101mJltypij8Hod3M2zfYGu6XRIRZGbxeJQ9uKlW2a_3S56xVOJmA1Yb3SEsKFZqtv3DaNYcMrmhZHmUMWojr9NvTBuBLhyHCd5hHa1ZsYSbt7G4nMhEEDL32U4DxjO7V7yvmJPXJTBuCiTGh3rUPjpYM_V0PJJG7TIaKp3XF0WiKi9pRXPegTztZoXor_fmWoFrK03McpLC-Yc_t0IigV6-Q88rZb0g9VPokC8gJ3ttE15maGKla4NPWI_U&lib=MbpKbbfePtAVndrs259dhPT7ROjQYJ8yx',
         success: function (responseText) {
-            var data = responseText.Response;
-            for (var i = 0; i < data.length; i++) {
+            var data = responseText.Responses
+            
+            //reset namelist and questionlist
+            nameList = [];
+            questionList = [];
+            
+            for (i = 0; i < data.length; i++) {
+                if (data[i].Display.length > 0) {
+                    if (data[i].Name.trim() == "" || data[i].Name.trim() == "-" || data[i].Name.trim() == ".")
+                        nameList.push("Anonymous");
+                    else {
+                        nameList.push(data[i].Name)
+                    }
 
+                    questionList.push(data[i].Question)
+                }
             }
-            //after all the variables are added accordingly, this functions would populate the various fields of the data visualisation.
-
+            displayQuestions(nameList, questionList);
         }
     });
+}
+
+function displayQuestions(nameList, questionList) {
+    // reset. 
+    $(".container").html("");
+
+    // display stuff, display the last one first, then the first.
+    for (var i = questionList.length - 1; i >= 0; i--) {
+        $(".container").append("<div class=\"jumbotron\"><h1 class=\"question-question\">" + questionList[i] + "</h1><h3 class=\"question-author\"> -&nbsp;" + nameList[i] + "</h3></div>");
+    }
+
+
 }
